a7aa4bc6010ba0fa6ef70c51f109c234d6590983
Sami Kukkonen <sami@samikukkonen.fi>
2021-05-03 20:14:05 +0300

    Handle SIGINT and SIGTERM signals
    Since distroless runs CMD processes as uid 1, signal handlers are not
    installed out of the box. This makes it possible to send SIGTERM to a
    running server process and cause it to gracefully exit.

--- dwk-server/src/main.rs
+++ dwk-server/src/main.rs
@@ -1,10 +1,26 @@
+use futures::future::select;
+use tokio::pin;
+use tokio::signal::unix::{signal, SignalKind};
+use tokio::sync::oneshot;
 use warp::Filter;
 use warp::http::StatusCode;
+
 use std::str::FromStr;
 use std::net::{SocketAddr, Ipv6Addr};
 use std::env;
 
 const DEFAULT_PORT: u16 = 80;
+
+async fn signal_handler(notification: oneshot::Sender<()>) {
+    let mut sigint = signal(SignalKind::interrupt()).unwrap();
+    let sigint = sigint.recv();
+    let mut sigterm = signal(SignalKind::terminate()).unwrap();
+    let sigterm = sigterm.recv();
+    pin!(sigint);
+    pin!(sigterm);
+    select(sigint, sigterm).await;
+    notification.send(()).unwrap();
+}
 
 #[tokio::main]
 pub async fn main() {
@@ -12,13 +28,18 @@ pub async fn main() {
         Ok(port) => u16::from_str_radix(&port, 10).unwrap(),
         Err(_) => DEFAULT_PORT
     };
+    let (tx, rx) = oneshot::channel();
+    tokio::task::spawn(signal_handler(tx));
     let route = warp::any().and_then(|| async {
         Err::<StatusCode, _>(warp::reject::not_found())
     });
-    let server = warp::serve(route).bind(
-        SocketAddr::new(Ipv6Addr::from_str("::").unwrap().into(), port)
+    let (addr, server) = warp::serve(route).bind_with_graceful_shutdown(
+        SocketAddr::new(Ipv6Addr::from_str("::").unwrap().into(), port),
+        async {
+            rx.await.ok();
+        }
     );
     let t = tokio::task::spawn(server);
-    println!("Server started in port {}", port);
+    println!("Server started in port {}", addr.port());
     t.await.unwrap();
 }
