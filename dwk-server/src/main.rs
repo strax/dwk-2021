5866a858aa965ec820bacf64e3867ca2077af668
Sami Kukkonen <sami@samikukkonen.fi>
2021-05-03 19:12:13 +0300

    Exercise 1.02

--- dwk-server/src/main.rs
+++ dwk-server/src/main.rs
@@ -1,11 +1,24 @@
-use tokio::time::{sleep, Duration};
-use uuid::Uuid;
+use warp::Filter;
+use warp::http::StatusCode;
+use std::str::FromStr;
+use std::net::{SocketAddr, Ipv6Addr};
+use std::env;
+
+const DEFAULT_PORT: u16 = 80;
 
 #[tokio::main]
 pub async fn main() {
-    loop {
-        let uuid = Uuid::new_v4();
-        println!("{}", uuid);
-        sleep(Duration::from_secs(5)).await;
-    }
+    let port = match env::var("PORT") {
+        Ok(port) => u16::from_str_radix(&port, 10).unwrap(),
+        Err(_) => DEFAULT_PORT
+    };
+    let route = warp::any().and_then(|| async {
+        Err::<StatusCode, _>(warp::reject::not_found())
+    });
+    let server = warp::serve(route).bind(
+        SocketAddr::new(Ipv6Addr::from_str("::").unwrap().into(), port)
+    );
+    let t = tokio::task::spawn(server);
+    println!("Server started in port {}", port);
+    t.await.unwrap();
 }
