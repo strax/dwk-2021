0a26058faca60b1ff2c76e392fd8b95c6d63d651
Sami Kukkonen <sami@samikukkonen.fi>
2021-05-04 18:24:50 +0300

    Add request tracing

--- dwk-server/src/main.rs
+++ dwk-server/src/main.rs
@@ -2,12 +2,15 @@ use futures::future::select;
 use tokio::pin;
 use tokio::signal::unix::{signal, SignalKind};
 use tokio::sync::oneshot;
+use tracing::{info, Level};
 use uuid::Uuid;
 use warp::Filter;
+use serde::{Serialize};
 
 use std::str::FromStr;
 use std::net::{SocketAddr, Ipv6Addr};
 use std::env;
+use tracing_subscriber::{EnvFilter};
 
 const DEFAULT_PORT: u16 = 80;
 
@@ -22,8 +25,17 @@ async fn signal_handler(notification: oneshot::Sender<()>) {
     notification.send(()).unwrap();
 }
 
+#[derive(Debug, Serialize)]
+struct ResponseEnvelope {
+    process_id: Uuid,
+    request_id: Uuid
+}
+
 #[tokio::main]
 pub async fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_env("LOG_LEVEL").add_directive(Level::INFO.into()))
+        .init();
     let port = match env::var("PORT") {
         Ok(port) => u16::from_str_radix(&port, 10).unwrap(),
         Err(_) => DEFAULT_PORT
@@ -32,10 +44,12 @@ pub async fn main() {
     let (tx, rx) = oneshot::channel();
     tokio::task::spawn(signal_handler(tx));
 
-    let route = warp::get().map(move || {
-        let request_id = Uuid::new_v4();
-        format!("ProcessId={} RequestId={}", pid, request_id)
-    });
+    let route = warp::get()
+        .map(move || {
+            let request_id = Uuid::new_v4();
+            warp::reply::json(&ResponseEnvelope { process_id: pid, request_id })
+        })
+        .with(warp::trace::request());
 
     let (addr, server) = warp::serve(route).bind_with_graceful_shutdown(
         SocketAddr::new(Ipv6Addr::from_str("::").unwrap().into(), port),
@@ -44,6 +58,6 @@ pub async fn main() {
         }
     );
     let t = tokio::task::spawn(server);
-    println!("Server started in port {}", addr.port());
+    info!(%addr, "online");
     t.await.unwrap();
 }
