902255bbb42aa8b6e0ad47a919f3b359e11d42b6
Sami Kukkonen <sami@samikukkonen.fi>
2021-05-04 21:27:48 +0300

    Tweak tracing output

--- dwk-server/src/main.rs
+++ dwk-server/src/main.rs
@@ -3,14 +3,14 @@ use tokio::pin;
 use tokio::signal::unix::{signal, SignalKind};
 use tokio::sync::oneshot;
 use tracing::{info, Level};
-use uuid::Uuid;
+use tracing_subscriber::{EnvFilter};
 use warp::Filter;
-use serde::{Serialize};
 
 use std::str::FromStr;
-use std::net::{SocketAddr, Ipv6Addr};
+use std::net::{SocketAddr, IpAddr};
 use std::env;
-use tracing_subscriber::{EnvFilter};
+
+mod app;
 
 const DEFAULT_PORT: u16 = 80;
 
@@ -25,39 +25,26 @@ async fn signal_handler(notification: oneshot::Sender<()>) {
     notification.send(()).unwrap();
 }
 
-#[derive(Debug, Serialize)]
-struct ResponseEnvelope {
-    process_id: Uuid,
-    request_id: Uuid
-}
-
 #[tokio::main]
 pub async fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_env("LOG_LEVEL").add_directive(Level::INFO.into()))
+        .without_time()
         .init();
     let port = match env::var("PORT") {
         Ok(port) => u16::from_str_radix(&port, 10).unwrap(),
         Err(_) => DEFAULT_PORT
     };
-    let pid = Uuid::new_v4();
     let (tx, rx) = oneshot::channel();
     tokio::task::spawn(signal_handler(tx));
 
-    let route = warp::get()
-        .map(move || {
-            let request_id = Uuid::new_v4();
-            warp::reply::json(&ResponseEnvelope { process_id: pid, request_id })
-        })
-        .with(warp::trace::request());
-
-    let (addr, server) = warp::serve(route).bind_with_graceful_shutdown(
-        SocketAddr::new(Ipv6Addr::from_str("::").unwrap().into(), port),
-        async {
+    let routes = app::routes::status().with(warp::trace::request());
+    let (addr, server) = warp::serve(routes)
+        .bind_with_graceful_shutdown(SocketAddr::new(IpAddr::from_str("::").unwrap(), port), async {
             rx.await.ok();
-        }
-    );
+        });
     let t = tokio::task::spawn(server);
     info!(%addr, "online");
     t.await.unwrap();
+    info!("shutdown");
 }
