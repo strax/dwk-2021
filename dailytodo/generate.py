06c211a95dbeeda41bf51b5a73a74f4cc4f43c09
Sami Kukkonen <sami@samikukkonen.fi>
2021-05-30 12:22:22 +0300

    Move dailytodo to dwk-exercises namespace
    We can access project API from another namespace via a more
    qualified DNS name (in this case, 'api.dwk-exercises.svc').

--- dailytodo/generate.py
+++ dailytodo/generate.py
@@ -6,6 +6,6 @@ uri = r.headers['location']
 print("URI: {}".format(uri))
 
 text = "Read {}".format(uri)
-r = requests.post("http://api/todos", json={"text": text})
+r = requests.post("http://api.dwk-project.svc/todos", json={"text": text})
 r.raise_for_status()
 print(r.json())
